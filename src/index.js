07faebb09857d5e61e32d249a1aeb3e004d1c5eb
AnthonyGW <anthony.waithaka@andela.com>
2017-09-26 16:35:48 +0300

    feat(bucketlistitems): add route for bucketlist items
    - also rename some routes and links to begin with /auth/

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-// import history from './history';
 
-// import Nav from './Components/nav';
 import Home from './pages/home';
 import Register from './pages/register';
 import Login from './pages/login';
 import UserBucketlists from './pages/bucketlists';
+import BucketlistItems from './pages/bucketlist_items';
 import * as authactions from './actions/authactions';
 
 const app = document.getElementById('root');
@@ -15,6 +14,7 @@ const app = document.getElementById('root');
 class Point extends React.Component{
     handleLogout(){
         authactions.logout();
+        window.location = '/auth/login';
     }
     render(){
     return(
@@ -22,14 +22,17 @@ class Point extends React.Component{
         <div>
             <h1>Bucket List Ultima</h1>
             {/* <Link to="/">Home .</Link> */}
-            <Link to="/register">. Create Account .</Link>
-            <Link to="/login">. Log In .</Link>
+            <Link to="/auth/register">. Create Account .</Link>
+            <Link to="/auth/login">. Log In .</Link>
             <Link to="/bucketlists">. Bucketlists .</Link>
             <button onClick={()=>{this.handleLogout()}}>Log out</button>
             <Route exact path="/" component={Home} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+            <Route exact path="/auth/register" component={Register} />
+            <Route exact path="/auth/login" component={Login} />
+            <Route path="/auth/logout" />
+            <Route path="/auth/reset-password" />
             <Route exact path="/bucketlists/" component={UserBucketlists} />
+            <Route path="/bucketlists/:bucketlistId/items/" component={BucketlistItems} />
         </div>
     </Router>);
     };
