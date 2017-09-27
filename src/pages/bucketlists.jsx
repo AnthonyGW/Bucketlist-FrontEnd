29c516c3a219fad29af809204c6ef2a4a3915b84
AnthonyGW <anthony.waithaka@andela.com>
2017-09-27 12:26:46 +0300

    feat(all): add edit bucketlist modal
    - store form input in state
    - handle form submit for both create and edit in the same method
    - handle modal open and close through method and state

--- src/pages/bucketlists.jsx
+++ src/pages/bucketlists.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 import Bucketlist from '../components/bucketlist';
 import BucketlistStore from '../stores/BucketlistStore';
@@ -11,7 +12,9 @@ export default class UserBucketlists extends React.Component{
             user_bucketlists: [],
             name: "",
             date: "",
-            description: ""
+            description: "",
+            show_modal: false,
+            editid: null,
         };
     }
     componentWillMount(){
@@ -34,18 +37,43 @@ export default class UserBucketlists extends React.Component{
             "date" : this.state.date,
             "description": this.state.description
         }
-        //throw in the token and call for a create-bucketlist action
-        bucketlistactions.createBucketlist(payload);
+        if(this.state.editid){
+            bucketlistactions.editBucketlist(this.state.editid, payload);
+            this.close();
+        }else{
+            //throw in the token and call for a create-bucketlist action
+            bucketlistactions.createBucketlist(payload);
+        }        
     }
     handleDelete(id){
-        console.log("onclick", id);
         bucketlistactions.deleteBucketlist(String(id));
     }
     handleView(id, name){
         //route to the retrieved list view
         var url = '/bucketlists/'+id+'/items';
         localStorage.setItem('list_id', id);
+        localStorage.setItem('list_name', name);
         window.location = url;
+    }
+    handleEdit(id, name, date, description, cond){
+        //open modal with the edit-bucketlist form
+        this.setState({
+            show_modal: cond,
+            editid: id,
+            name: name,
+            date: date,
+            description: description
+        });
+    }
+    close(){
+        this.setState({
+            show_modal: false,
+            editid: null,
+            name: "",
+            date: "",
+            description: ""
+        });
+        document.getElementById("create-list-form").reset();
     }
     render(){
         let bucketlists = this.state.user_bucketlists;
@@ -54,7 +82,7 @@ export default class UserBucketlists extends React.Component{
             return(
                 <div key={bucketlist.id}>
                     <Bucketlist key={bucketlist.id} bucketlistdata={bucketlist} />
-                    <button onClick={()=>{this.handleView(bucketlist.id, bucketlist.name)}}>View</button> | <button>Edit</button> | <button onClick={()=>{this.handleDelete(bucketlist.id)}}>Delete</button>
+                    <button onClick={()=>{this.handleView(bucketlist.id, bucketlist.name)}}>View</button> | <button onClick={()=>{this.handleEdit(bucketlist.id, bucketlist.name, bucketlist.date, bucketlist.description, true)}}>Edit</button> | <button onClick={()=>{this.handleDelete(bucketlist.id)}}>Delete</button>
                     <br />
                     ----------------------------------
                 </div>
@@ -63,12 +91,12 @@ export default class UserBucketlists extends React.Component{
         return (
             <div>
                 <h2>Create New Bucketlist</h2>
-                <form onSubmit={this.handleSubmit.bind(this)}>
+                <form id="create-list-form" onSubmit={this.handleSubmit.bind(this)}>
                     <label>Enter name:</label><br />
                     <input type="text" onChange={this.handleChange.bind(this)} id="name" placeholder="name" />
                     <br />
                     <label>Enter date:</label><br />
-                    <input type="text"  onChange={this.handleChange.bind(this)} id="date" placeholder="ddmmyyyy" />
+                    <input type="text" onChange={this.handleChange.bind(this)} id="date" placeholder="ddmmyyyy" />
                     <br />
                     <label>Enter description:</label><br />
                     <input type="text" onChange={this.handleChange.bind(this)} id="description" placeholder="description" />
@@ -78,6 +106,27 @@ export default class UserBucketlists extends React.Component{
                 <h1> My Bucketlists </h1>
                 <ul>{BucketlistArray}</ul>
                 <br />
+                <Modal show={this.state.show_modal} onHide={this.close.bind(this)}>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit {this.state.name}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <label>Enter name:</label><br />
+                        <input type="text" onChange={this.handleChange.bind(this)} id="name" placeholder={"name: "+this.state.name} />
+                        <br />
+                        <label>Enter date:</label><br />
+                        <input type="text" onChange={this.handleChange.bind(this)} id="date" placeholder={"ddmmyyyy: "+this.state.date} />
+                        <br />
+                        <label>Enter description:</label><br />
+                        <input type="text" onChange={this.handleChange.bind(this)} id="description" placeholder={"description: "+this.state.description} />
+                        <br />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type="submit">Submit</Button> | <Button onClick={this.close.bind(this)}>Close</Button>
+                    </Modal.Footer>
+                </form>
+                </Modal>
             </div>
         );
     }
